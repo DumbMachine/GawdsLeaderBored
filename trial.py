11741fe70654d584958157d116e7d96be92c0274
Ratin_Kumar <ratin.kumar.2k@gmail.com>
2018-11-25 13:05:51 +0530

    Minor Changes

--- trial.py
+++ trial.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 def amazon_pricer(url):
+    tries = 0
     headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36'}
     try:
         res = requests.get(url,headers = headers,verify=True)
@@ -13,18 +14,17 @@ def amazon_pricer(url):
         x= x.replace("nth-child","nth-of-type")
         k = soup.select(x)
         try:
-            k= str(k[0])
-            print(k[k.find(">")+7:4])
-            return (k[41:41+3]) #k[41]
+          k= str(k[0])
+          print(k[k.find(">")+7:4])
+          return (k[41:41+3]) #k[41]
         except:
-            print("err")
-            return
+          return ("err")
         
     else:
         return "Site nein Diya Dhokha, Ye abhi nahi Hoga"
 
-k = amazon_pricer("https://github.com/DumbMachine")
-usernames = ["DumbMachine","war-turtle","nkkumawat","MacBox7","avinashbharti97","phoenixking25"]
+k = amazon_pricer("https://github.com/nkkumawat")#https://github.com/nkkumawat
+usernames = ["DumbMachine","war-turtle","nkkumawat","AnshulMalik","EUNIX-TRIX","Varun kumar","MacBox7","avinashbharti97","phoenixking25"]
 for name in usernames:
     link = "https://github.com/{}".format(name)
     print(amazon_pricer(link))
